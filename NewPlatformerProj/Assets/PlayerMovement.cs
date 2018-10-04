6864bcf7a14228bd3f56d8e96dda1aa4e127b338
Ethan Malenchek <Ethan@Ethans-MacBook-Pro-2.local>
2018-10-03 20:45:58 -0400

    commited assets and c# template files as well as CharacterController, need to finish PlayerMovement Script.

---
+++ NewPlatformerProj/Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController2D controller;
+
+    float horizontal = 0f;
+
+    public float runSpd = 40f;
+
+	// Update is called once per frame
+	void Update () {
+
+        horizontal = Input.GetAxisRaw("Horizontal") * runSpd;
+	}
+
+    private void FixedUpdate()
+    {
+        // Moves Character
+        controller.Move(horizontal * Time.fixedDeltaTime, false, false);
+    }
+}
